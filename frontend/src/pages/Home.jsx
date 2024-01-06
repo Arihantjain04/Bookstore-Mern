0f4d0a5dc1f861892da17238dbf8ff89252f476a
Arihant Jain <127401868+Arihantjain04@users.noreply.github.com>
2024-01-06 13:32:12 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5557/books")
+      .get("https://bookstore-app-mern.vercel.app/books")
       .then((res) => {
         setBooks(res.data.data);
         setLoading(false);
